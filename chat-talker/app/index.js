a18a8985be0ead7c71aa4df06325ece93877fb46
David Poindexter <drpoindexter@gmail.com>
2019-07-11 16:27:27 -0400

    Initial working twitch chat read-only connection

--- chat-talker/app/index.js
+++ chat-talker/app/index.js
@@ -1,1 +1,20 @@
-console.log('HELLO WORLD, SO TYPICAL');
+const ComfyJS = require('comfy.js');
+
+ComfyJS.onCommand = (user, command, message, flags) => {
+  if (command === 'number1') {
+    console.log(
+      `${user} Number 1, I order you to take a Number 2, and also ${message}`
+    );
+  }
+  if (command === 'hello') {
+    console.log(`It's me..., ${user}`);
+  }
+  if (command === 'wut' || command === 'what') {
+    console.log(`Say what again. I dare you. I double dare you.`);
+  }
+  if (command === 'wallace') {
+    console.log(`${user}... What does Marsallace Wallace look like?`);
+  }
+};
+
+ComfyJS.Init('roberttables');
